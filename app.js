154522e3f5055c0ae580092888d2f24174519c81
Oleh Petraniuk <olehshort@gmail.com>
2018-11-30 00:08:43 +0100

    User Model, Add User & Register

--- app.js
+++ app.js
@@ -7,37 +7,35 @@ const mongoose = require('mongoose');
 const config = require('./config/database');
 
 // Connect To Database
+mongoose.connect(config.database);
 
-mongoose.connect('mongodb://localhost:27017/meanauth', { useNewUrlParser: true });
-
-
-// on Connection
+// On Connection
 mongoose.connection.on('connected', () => {
-    console.log('Connected to database', + config.database);
+  console.log('Connected to database '+config.database);
 });
 
 // On Error
 mongoose.connection.on('error', (err) => {
-    console.log('Database error: '+err);
-  });
+  console.log('Database error: '+err);
+});
 
 const app = express();
 
 const users = require('./routes/users');
 
-//Port number
+// Port Number
 const port = 3000;
 
-//CORS Middleewar
+// CORS Middleware
 app.use(cors());
 
-//Set Static Folder
+// Set Static Folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-//Body Parser Middleware
+// Body Parser Middleware
 app.use(bodyParser.json());
 
-//Passport Middleware
+// Passport Middleware
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -45,12 +43,12 @@ require('./config/passport')(passport);
 
 app.use('/users', users);
 
-//Index Route
+// Index Route
 app.get('/', (req, res) => {
-    res.send('Invalid Endpoint');
+  res.send('Invalid Endpoint');
 });
 
-//Start Server
+// Start Server
 app.listen(port, () => {
-    console.log('Server started op port ' + port);
+  console.log('Server started on port '+port);
 });

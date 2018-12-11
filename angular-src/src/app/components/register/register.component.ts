e532ca70fc621c55cf9bd1fa526466ff5353135c
Oleh Petraniuk <olehshort@gmail.com>
2018-12-11 22:20:24 +0100

    Add Auth Service

--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ValidateService} from '../../services/validate.service'
+import {AuthService} from '../../services/auth.service'
 import {FlashMessagesService} from 'angular2-flash-messages';
 
 @Component({
@@ -13,7 +14,11 @@ export class RegisterComponent implements OnInit {
   email: String;
   password: String;
 
-  constructor(private validateService: ValidateService, private flashMessage:FlashMessagesService) { }
+  constructor(
+    private validateService: ValidateService, 
+    private flashMessage:FlashMessagesService,
+    private authService: AuthService
+    ) { }
 
   ngOnInit() {
   }
@@ -37,6 +42,15 @@ export class RegisterComponent implements OnInit {
       this.flashMessage.show('Please use a valid email', {cssClass: 'alert-danger', timeout: 3000});
       return false;
     }
+
+    // Register User
+    this.authService.registerUser(user).subscribe(data => {
+      if(data.success) {
+
+      } else {
+        
+      }
+    });
   }
 
 }

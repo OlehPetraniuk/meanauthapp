26633ea7283d0ed9a7d93d2b14ab60ef42d2bdd0
Oleh Petraniuk <olehshort@gmail.com>
2018-12-12 08:51:28 +0100

    Add User Registration

--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {ValidateService} from '../../services/validate.service'
 import {AuthService} from '../../services/auth.service'
 import {FlashMessagesService} from 'angular2-flash-messages';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -17,7 +18,8 @@ export class RegisterComponent implements OnInit {
   constructor(
     private validateService: ValidateService, 
     private flashMessage:FlashMessagesService,
-    private authService: AuthService
+    private authService: AuthService,
+    private router: Router
     ) { }
 
   ngOnInit() {
@@ -46,9 +48,11 @@ export class RegisterComponent implements OnInit {
     // Register User
     this.authService.registerUser(user).subscribe(data => {
       if(data.success) {
-
+        this.flashMessage.show('You are now registered and can log in', {cssClass: 'alert-success', timeout: 3000});
+        this.router.navigate(['/login']);
       } else {
-        
+        this.flashMessage.show('Something went wrong', {cssClass: 'alert-danger', timeout: 3000});
+        this.router.navigate(['/register']);
       }
     });
   }
